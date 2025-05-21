de7bcb6b6b2c189150438dbf62bad66df70cd538
Roei Bajayo <roei.bajayo@ewave.co.il>
2025-05-21 23:40:55 +0300

    update Benchmarks

--- tests/MemoryCore.Benchmarks/Benchmark.cs
+++ tests/MemoryCore.Benchmarks/Benchmark.cs
@@ -1,22 +1,30 @@
 ﻿using BenchmarkDotNet.Attributes;
+using BitFaster.Caching.Lru;
 using System.Runtime.Caching;
 
 namespace MemoryCore.Benchmarks;
 
 [MemoryDiagnoser(false)]
+[RankColumn]
+[ShortRunJob]
+[MinColumn, MaxColumn, MedianColumn]
+[MarkdownExporterAttribute.GitHub]
 public class Benchmark
 {
     private MemoryCache? memoryCache;
     private MemoryCoreManager? memoryCore;
+    private ConcurrentLru<string, object>? lur;
 
     [GlobalSetup]
     public void Setup()
     {
         memoryCache = new MemoryCache("global");
         memoryCore = new MemoryCoreManager();
+        lur = new ConcurrentLru<string, object>(128);
 
         memoryCore.Add("global-key", "value", TimeSpan.FromMinutes(10));
         memoryCache.Add("global-key", "value", DateTimeOffset.Now.AddMinutes(10));
+        lur.AddOrUpdate("global-key", "value");
     }
 
     [Benchmark]
@@ -24,11 +32,15 @@ public class Benchmark
     {
         memoryCore!.Add("key-for-add", "value", TimeSpan.FromMinutes(1));
     }
-
     [Benchmark]
-    public void MemoryCache_Add()
+    public bool MemoryCache_Add()
     {
-        memoryCache!.Add("key-for-add", "value", DateTimeOffset.Now.AddMinutes(1));
+        return memoryCache!.Add("key-for-add", "value", DateTimeOffset.Now.AddMinutes(1));
+    }
+    [Benchmark]
+    public void ConcurrentLru_Add()
+    {
+        lur!.AddOrUpdate("key-for-add", "value");
     }
 
     [Benchmark]
@@ -36,11 +48,16 @@ public class Benchmark
     {
         return memoryCore!.Get<string>("global-key");
     }
-
     [Benchmark]
     public string MemoryCache_Get()
     {
         return (string)memoryCache!.Get("global-key");
+    }
+    [Benchmark]
+    public string? ConcurrentLru_Get()
+    {
+        _ = lur!.TryGet("global-key", out var value);
+        return (string)value!;
     }
 
 
@@ -49,10 +66,30 @@ public class Benchmark
     {
         return memoryCore!.Exists("global-key");
     }
-
     [Benchmark]
     public bool MemoryCache_Exists()
     {
         return memoryCache!.Any(x => x.Key == "global-key");
+    }
+    [Benchmark]
+    public bool ConcurrentLru_Exists()
+    {
+        return lur!.TryGet("global-key", out _);
+    }
+
+    [Benchmark]
+    public void MemoryCore_Remove()
+    {
+        memoryCore!.Remove("global-key");
+    }
+    [Benchmark]
+    public object MemoryCache_Remove()
+    {
+        return memoryCache!.Remove("global-key");
+    }
+    [Benchmark]
+    public bool ConcurrentLru_Remove()
+    {
+        return lur!.TryRemove("global-key");
     }
 }
